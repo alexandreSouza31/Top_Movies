d59bd77f99be40308013dd6adfdd24ee5e03e068
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-02 13:10:19 -0300

    will create the system routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+
+import Home from './pages/Home.jsx'
+import Movie from './pages/Movie.jsx'
+import Search from './pages/Search.jsx'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>{/*será o componente pai da aplicação */}
+
+          {/* definirei as rotas do projeto: */}
+          <Route path='/' element={<Home />} />
+          <Route path='/movie/:id' element={<Movie />} />
+          <Route path='/search' element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
