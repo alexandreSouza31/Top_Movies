65c8b8bd7aa8dc37021d49bcf338c2488168db3b
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-03 19:38:42 -0300

    refact: will refactor the way to display movies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,29 +6,37 @@ const key = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
     const [popMovies, setPopMovies] = useState([])
+    //const [topMovies, setTopMovies] = useState([])
 
-    const getPopMovies = async (url) => {
-        const res = await fetch(url);
-        const data = await res.json();
-
-        console.log(data)
-        setPopMovies(data.results);
+    const getMovies = async (url,state) => {//o state é passado como parâmetro pra conseguir usar mais de um
+        const res = await fetch(url)
+        const data = await res.json()
+        
+        state(data.results)
     }
+
+    const createUrl = (category) => `${ url}${category}${key}`
 
     useEffect(() => {/*tenho possibilidade de executar uma função em alguns 
     estágios da minha aplicação(baseado no array de dependências no final da função '[]')*/
 
-        const popMoviesUrl = `${url}popular?${key}`//aqui eu monto a url
-        getPopMovies(popMoviesUrl)
+        const popMoviesUrl =createUrl('popular?')//aqui eu monto a url de acordo com a api
+        //const topMoviesUrl =createUrl('top_rated?')//aqui eu monto a url de acordo com a api
+        getMovies(popMoviesUrl,setPopMovies)
+        //getMovies(topMoviesUrl,setTopMovies)
 
-    }, [])//array vazio: será executado somente quando a pág for carregada 
+    }, [])//array vazio: será executado somente quando a pág for carregada
 
+    const mapPop = popMovies.length > 0 &&popMovies.map((pop) => <p>"populares"{pop.title}</p>)
+    //const mapTop = topMovies.length > 0 && topMovies.map((top) => <p>{top.title}</p>)
+    
     return (
         <div className="container">
-            <h2 className="title">Filmes mais populares: </h2>
+            
             <div className="movies_container">
-                {popMovies.length===0 && <p>Carregando...</p>}
-                {popMovies.length > 0 && popMovies.map((movie) => <p>{movie.title}</p>)}
+                {popMovies.length === 0 && <p>Carregando...</p>}
+                {mapPop}
+                
             </div>
         </div>
     )
