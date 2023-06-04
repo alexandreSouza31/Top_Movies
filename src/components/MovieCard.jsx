138489c0d3b73b8507e93bb0e18cbc3a3ebf2fc4
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-04 17:10:51 -0300

    will style the details link

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,20 +6,19 @@ const imageUrl = import.meta.env.VITE_API_IMG;
 
 const MovieCard = ({ movie, showLink = true }) => {
     return (
-        <div>
+        <div className="container">
             <div className="img-container">
                 <Link to={`/movie/${movie.id}`}>
                     <img src={imageUrl + movie.poster_path} alt={movie.title} />
                 </Link>
             </div>
-            <div className="details">
-                <h3>{movie.title}</h3>
-                {showLink &&
-                    <Link to={`/movie/${movie.id}`}>{/*mesmo estilo da url do main.jsx*/}
-                        Detalhes
+            <h3>{movie.title}</h3>
+            <div className="link-movie">
+            {showLink &&
+                <Link to={`/movie/${movie.id}`}>{/*mesmo estilo da url do main.jsx*/}
+                    Detalhes
                     </Link>}
             </div>
-
         </div>
     )
 }
