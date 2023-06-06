5f871a2a75fe7f986bbf3c493c277f6f8773b786
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-06 12:31:58 -0300

    will apply the coin shape hook

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import getMovies from "../hooks/getMovies"
- 
+
 import {
     BsGraphUp,
     BsWallet2,
@@ -12,6 +12,7 @@ import {
 import MovieCard from "../components/MovieCard";
 
 import "./Movie.css";
+import formatCurrency from "../hooks/formatCurrency";
 
 const url = import.meta.env.VITE_API_URL;
 const key = import.meta.env.VITE_API_KEY;
@@ -21,10 +22,11 @@ const Movie = () => {
     const { id } = useParams()//vai pegar o id que está na url desestruturando e usando hook.
     const [movie, setMovie] = useState(null);
 
-    useEffect(() => {
-        const createUrl = `${url}${id}?${key}`;
+    //const createUrl = `${url}${id}?${key}`;
+    const createUrlPt = `${url}${id}?${key}&language=pt-BR `;
 
-        getMovies(createUrl, setMovie, false)
+    useEffect(() => {
+        getMovies(createUrlPt, setMovie, false)
     }, [])
 
     return (
@@ -35,22 +37,22 @@ const Movie = () => {
                     <p className="tagline">{movie.tagline}</p>
 
                     <div className="info">
-                        <h3><BsWallet2/>Orçamento:</h3>
+                        <h3><BsWallet2 />Orçamento:</h3>
                     </div>
-                    <p>{movie.budget}</p>
+                    <p>{formatCurrency(movie.budget,"en-US","USD")}</p>
 
                     <div className="info">
-                        <h3><BsGraphUp/>Receita:</h3>
+                        <h3><BsGraphUp />Receita:</h3>
                     </div>
-                    <p>{movie.revenue}</p>
+                    <p>{formatCurrency(movie.revenue,"en-US","USD")}</p>
 
                     <div className="info">
-                        <h3><BsHourglassSplit/>Duração:</h3>
+                        <h3><BsHourglassSplit />Duração:</h3>
                     </div>
                     <p>{movie.runtime} minutos</p>
 
                     <div className="info description">
-                        <h3><BsFillFileEarmarkTextFill/>Descrição:</h3>
+                        <h3><BsFillFileEarmarkTextFill />Descrição:</h3>
                     </div>
                     <p>{movie.overview}</p>
                 </>
