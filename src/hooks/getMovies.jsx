7cdd962ef0d1c6bb67ccbdaaf31f89251b7e4085
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-06 01:06:34 -0300

    will add a new parameter to the hook

--- src/hooks/getMovies.jsx
+++ src/hooks/getMovies.jsx
@@ -1,13 +1,13 @@
-
-
-
-    const getMovies =async (url,state) => {//o state é passado como parâmetro pra conseguir usar mais de um
+    const getMovies =async (url,state,results=false) => {//o state é passado como parâmetro pra conseguir usar mais de um
         const res = await fetch(url)
         const data = await res.json()
         
-        state(data.results)
+        !results?state(data):state(data.results)
+     /* A página que é mostrada após clicar em detalhes não
+      tem "data.results", como exite na home e na search, por
+      exemplo, mas sim somente "data". Então criei essa função, e 
+      quando ela é chamada em determinada página passo o
+      booleano como argumento. Se tem resultado, true, senão, false. */
     }
    
-
-
 export default getMovies
